04f38ac7bb56bfafc795f3f560faa1ada65efc02
Saul Reynolds-Haertle <saulrh@gatech.edu>
2015-02-10 11:33:02 -0500

    change over to using sparse matrices.
    new scheme will require a matrix with a number of columns linear in the
    number of materials and edges and a number of rows linear in the number
    of edges in the dependency graph. This is unfortunately dogawful,
    because, really, O(ve + e^2)? Eurgh. Thankfully, sparse matrices to the
    rescue! An overwhelmingly vast majority of that matrix is zero because
    the our linear equations are never going to have more than eight or ten
    nonzero coefficients each while we have thousands of equations. Which
    means our matrix is lolsparse and our complexities are back into the
    realm of being linear in the number of edges. Yay math!

--- oreganizer-lp.py
+++ oreganizer-lp.py
@@ -1,31 +1,40 @@
 #!/usr/bin/env python3
 
-from cvxopt import matrix, solvers
+from cvxopt import spmatrix, matrix, solvers
 import itertools as it
-import tabulate
 
-A_rows = []
-b_rows = []
-G_rows = []
-h_rows = []
-c_rows = []
+# i is rows, j is columns
 
-def buildrow(terms):
-    row = [0] * len(indices)
-    for var,coeff in terms:
-        row[indices[var]] = coeff
-    return row
+A_x = []
+A_i = []
+A_j = []
 
-def addeq(const, terms):
-    row = buildrow(terms)
-    A_rows.append(row)
-    b_rows.append(const)
+G_x = []
+G_i = []
+G_j = []
 
-def addineq(const, terms):
-    row = buildrow(terms)
-    G_rows.append(row)
-    h_rows.append(const)
+h_dict = dict()
+c_dict = dict()
+b_dict = dict()
 
+eqrows = dict()
+ineqrows = dict()
+
+def addeq(name, const, terms):
+    eqrows[name] = len(eqrows)
+    for var, coeff in terms:
+        A_i.append(eqrows[name])
+        A_j.append(indices[var])
+        A_x.append(coeff)
+    b_dict[eqrows[name]] = const
+
+def addineq(name, const, terms):
+    ineqrows[name] = len(ineqrows)
+    for var, coeff in terms:
+        G_i.append(ineqrows[name])
+        G_j.append(indices[var])
+        G_x.append(coeff)
+    h_dict[ineqrows[name]] = const
 
 mats = ["sglass",
         "sbrick",
@@ -37,26 +46,37 @@ mats = ["sglass",
 mat_vars = ["_w", "_n", "_h", "_e"]
 variables = it.product(mats, mat_vars)
 variables = ["".join(x) for x in variables]
+
+# our final dict: maps variable names to their columns
 indices = dict(zip(variables, it.count(0)))
 
-addeq(1., [("sglass_w", 1./5.)])
-addeq(0., [("sglass_n", -1.),
+addeq("sglass top",
+      1., [("sglass_w", 1./5.)])
+addeq("sglass-n glass",
+      0., [("sglass_n", -1.),
            ("glass_w", 1./5.)])
-addeq(0., [("sglass_n", -1.),
+addeq("sglass-n sbrick",
+      0., [("sglass_n", -1.),
            ("sbrick_w", 1./4.)])
-addeq(0., [("sbrick_n", -1.),
+addeq("sbrick-n grout",
+      0., [("sbrick_n", -1.),
            ("grout_w", 1.)])
-addeq(0., [("glass_n", -1.),
+addeq("glass-n sand",
+      0., [("glass_n", -1.),
            ("sand_w", 1.)])
-addeq(0., [("grout_n", -1.),
+addeq("grout-n sand",
+      0., [("grout_n", -1.),
            ("sand_w", 1.)])
-addeq(0., [("grout_n", -1.),
+addeq("grout-n clay",
+      0., [("grout_n", -1.),
            ("clay_w", 1.)])
-addeq(0., [("grout_n", -1.),
+addeq("grout-n gravel",
+      0., [("grout_n", -1.),
            ("gravel_w", 1.)])
 
 for mat in mats:
-    addeq(0, [(mat + "_w", -1.),
+    addeq(mat + "-balance",
+          0, [(mat + "_w", -1.),
               (mat + "_n", 1.),
               (mat + "_h", 1.),
               (mat + "_e", -1)])
@@ -71,11 +91,14 @@ counts = [("sglass", 0.),
           ("clay", 0.)]
 
 for mat,count in counts:
-    addeq(count, [(mat + "_h", 1.)])
+    addeq(mat + "-have",
+          count, [(mat + "_h", 1.)])
 
 for mat in mats:
-    addineq(0., [(mat + "_w", -1.)])
-    addineq(0., [(mat + "_e", -1.)])
+    addineq(mat + "-w pos",
+            0., [(mat + "_w", -1.)])
+    addineq(mat + "-e pos",
+            0., [(mat + "_e", -1.)])
 
 overages = [("sglass", 1.),
             ("sbrick", 1.),
@@ -85,46 +108,34 @@ overages = [("sglass", 1.),
             ("gravel", 1.),
             ("clay", 1.)]
 
-c_rows = [0] * len(variables)
 for mat,cost in overages:
-    c_rows[indices[mat + "_e"]] = cost
-    c_rows[indices[mat + "_w"]] = cost
+    c_dict[indices[mat + "_e"]] = cost
+    c_dict[indices[mat + "_w"]] = cost
 
-print()
-print(tabulate.tabulate(A_rows))
+h_row = [0] * len(ineqrows)
+for idx, val in h_dict.items():
+    h_row[idx] = val
 
-print()
-print(tabulate.tabulate(G_rows))
+b_row = [0] * len(eqrows)
+for idx, val in b_dict.items():
+    b_row[idx] = val
 
-print()
-print(b_rows)
-print(h_rows)
-print()
-print(c_rows)
+c_row = [0] * len(variables)
+for idx, val in c_dict.items():
+    c_row[idx] = val
 
-c = matrix(c_rows)
-b = matrix(b_rows)
-G = matrix(G_rows).T
-A = matrix(A_rows).T
-h = matrix(h_rows)
 
+c = matrix(c_row)
+b = matrix(b_row)
+G = spmatrix(G_x, G_i, G_j)
+A = spmatrix(A_x, A_i, A_j)
+h = matrix(h_row)
+
+print(repr(h))
 
 sol = solvers.lp(c, G, h, A, b)
 
 numbers = [round(x, 3) for x in sol['x']]
-
-# for var in variables:
-#     print("{0:20}: {1}".format(var, numbers[indices[var]]))
-
-# v1 = "sglass_n"
-# for line in A_rows:
-#     if line[indices[v1]] != 0:
-#         print()
-#         for v2 in variables:
-#             if line[indices[v2]] != 0:
-#                 print("{0:20}: {1}".format(v2, line[indices[v2]]))
-            
-
 for mat in mats:
     print("{0:8}: want {1:5}, have {2:5}, so need {3:5} and {4:5} extra".format(
         mat,
